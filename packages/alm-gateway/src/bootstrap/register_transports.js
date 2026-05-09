ae575cd7178a766244d7b715081faef3d285f50d
رأفت عمر <shamwebs1@gmail.com>
2026-05-09 08:53:26 +0300

    Add SSH transport registration

---
+++ packages/alm-gateway/src/bootstrap/register_transports.js
@@ -0,0 +1,9 @@
+// packages/alm-gateway/src/bootstrap/register_transports.js
+
+const {
+  registerTransport
+} = require("alm-core/src/execution/transport_registry");
+
+const sshAdapter = require("../transports/ssh/ssh_adapter");
+
+registerTransport("ssh", sshAdapter);
