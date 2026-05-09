52bdb9b758fad0b9740decba738e5b7e1d25d060
رأفت عمر <shamwebs1@gmail.com>
2026-05-10 02:19:43 +0300

    Refactor loadProfile to use CommonJS syntax

--- stress/utils.js
+++ stress/utils.js
@@ -1,7 +1,10 @@
-// /stress/utils.js
-import fs from 'fs';
+// stress/utils.js
 
-export function loadProfile(name) {
+const fs = require("fs");
+
+function loadProfile(name) {
   const path = `./stress/profiles/${name}.json`;
-  return JSON.parse(fs.readFileSync(path, 'utf8'));
+  return JSON.parse(fs.readFileSync(path, "utf8"));
 }
+
+module.exports = { loadProfile };
