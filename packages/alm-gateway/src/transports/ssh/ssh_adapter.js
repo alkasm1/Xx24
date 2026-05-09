02f7251f57378f8bb8b2cbf7b386e5195dae00d3
رأفت عمر <shamwebs1@gmail.com>
2026-05-09 10:26:21 +0300

    Validate SSH descriptor for missing command

--- packages/alm-gateway/src/transports/ssh/ssh_adapter.js
+++ packages/alm-gateway/src/transports/ssh/ssh_adapter.js
@@ -4,12 +4,23 @@ const execSSH = require("./ssh_exec");
 
 module.exports = {
   async run(device, descriptor) {
+    if (
+      !descriptor.payload ||
+      !descriptor.payload.command
+    ) {
+      throw new Error(
+        "SSH descriptor missing payload.command"
+      );
+    }
+
     return execSSH({
       host: device.ip,
       port: device.port,
       username: device.username,
       password: device.password,
-      command: descriptor.command,
+
+      command: descriptor.payload.command,
+
       timeout: descriptor.timeout || 5000
     });
   }
