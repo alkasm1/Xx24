99e4f98515e986b1de45274a444c4c652b16ca4d
رأفت عمر <shamwebs1@gmail.com>
2026-05-10 02:20:31 +0300

    Refactor WebSocket initialization and export function

--- stress/transports/ws.js
+++ stress/transports/ws.js
@@ -1,11 +1,14 @@
-// /stress/transports/ws.js
-import WebSocket from 'ws';
+// stress/transports/ws.js
 
-const ws = new WebSocket('ws://localhost:9000');
+const WebSocket = require("ws");
 
-export function sendWS(payload) {
+const ws = new WebSocket("ws://127.0.0.1:5001");
+
+function sendWS(payload) {
   return new Promise((resolve, reject) => {
     if (ws.readyState !== 1) return reject();
     ws.send(JSON.stringify(payload), err => err ? reject(err) : resolve());
   });
 }
+
+module.exports = { sendWS };
