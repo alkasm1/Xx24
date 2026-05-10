8c46ff1abf1e08706c1ad91654d5039da47c7478
رأفت عمر <shamwebs1@gmail.com>
2026-05-11 00:33:41 +0300

    Update ws.js

--- stress/transports/ws.js
+++ stress/transports/ws.js
@@ -2,8 +2,6 @@
 
 const WebSocket = require("ws");
 
-const ws = new WebSocket("ws://127.0.0.1:5001");
-
 function sendWS(payload) {
   return new Promise((resolve, reject) => {
     if (ws.readyState !== 1) return reject();
