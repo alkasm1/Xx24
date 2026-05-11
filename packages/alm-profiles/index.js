ff2e554324d4f7377e438ca1aa63297cb15e6847
رأفت عمر <shamwebs1@gmail.com>
2026-05-11 19:58:14 +0300

    Refactor require statements and format code

--- packages/alm-profiles/index.js
+++ packages/alm-profiles/index.js
@@ -1,17 +1,67 @@
 // packages/alm-profiles/index.js
 
-const mikrotikSystem = require("./mikrotik/system");
-const mikrotikWifi = require("./mikrotik/wifi");
+const mikrotikSystem = require(
+  "./mikrotik/system"
+);
 
-const linuxSystem = require("./linux/system");
+const mikrotikWifi = require(
+  "./mikrotik/wifi"
+);
 
-module.exports = {
+const linuxSystem = require(
+  "./linux/system"
+);
+
+// -----------------------------
+// PROFILE REGISTRY
+// -----------------------------
+const profiles = {
   routeros: {
-    system: mikrotikSystem,
-    wifi: mikrotikWifi
+    system:
+      mikrotikSystem,
+
+    wifi:
+      mikrotikWifi
   },
 
   linux: {
-    system: linuxSystem
+    system:
+      linuxSystem
   }
+};
+
+// -----------------------------
+// HELPERS
+// -----------------------------
+function getProfile(
+  profile
+) {
+  return (
+    profiles[profile] ||
+    null
+  );
+}
+
+function hasProfile(
+  profile
+) {
+  return Boolean(
+    profiles[profile]
+  );
+}
+
+function listProfiles() {
+  return Object.keys(
+    profiles
+  );
+}
+
+module.exports = {
+  profiles,
+
+  getProfile,
+
+  hasProfile,
+
+  listProfiles
 };
