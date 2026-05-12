c7ef18cf44b7426cdf103ac4558a72d45d56568b
رأفت عمر <shamwebs1@gmail.com>
2026-05-12 14:04:57 +0300

    Create message_router.js

---
+++ frontend/runtime/message_router.js
@@ -0,0 +1,51 @@
+import {
+  handleSnapshot
+} from "./message_handlers/snapshot.js";
+
+import {
+  handleOpcodeResult
+} from "./message_handlers/opcode_result.js";
+
+import {
+  handleTaskUpdate
+} from "./message_handlers/task_update.js";
+
+import {
+  handleStressUpdate
+} from "./message_handlers/stress_update.js";
+
+import {
+  handleTerminal
+} from "./message_handlers/terminal.js";
+
+const handlers = {
+
+  snapshot:
+    handleSnapshot,
+
+  "opcode.result":
+    handleOpcodeResult,
+
+  "task.update":
+    handleTaskUpdate,
+
+  stressUpdate:
+    handleStressUpdate,
+
+  terminal:
+    handleTerminal
+};
+
+export function routeMessage(
+  msg
+) {
+
+  const handler =
+    handlers[msg.type];
+
+  if (!handler) {
+    return;
+  }
+
+  handler(msg);
+}
