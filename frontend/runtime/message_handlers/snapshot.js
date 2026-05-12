0c45713658a545369c5a704b2b33d3fb49f3b0ca
رأفت عمر <shamwebs1@gmail.com>
2026-05-12 14:08:57 +0300

    Create snapshot.js

---
+++ frontend/runtime/message_handlers/snapshot.js
@@ -0,0 +1,34 @@
+// frontend/runtime/message_handlers/snapshot.js
+
+import {
+  runtimeState
+} from "../state.js";
+
+import {
+  renderDevices
+} from "../../renderers/devices_renderer.js";
+
+export function handleSnapshot(
+  msg
+) {
+
+  runtimeState.snapshot =
+    msg;
+
+  renderDevices({
+    devices:
+      msg.devices,
+
+    metrics:
+      msg.metrics,
+
+    sessions:
+      msg.sessions,
+
+    activeTasks:
+      msg.activeTasks,
+
+    broadcasts:
+      msg.broadcasts
+  });
+}
