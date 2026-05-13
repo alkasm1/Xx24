34e83ca927728d0a9f9f78526e2c04b39103883e
رأفت عمر <shamwebs1@gmail.com>
2026-05-13 09:10:18 +0300

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,10 @@
 // frontend/app.js
 
 import {
+  executeTerminalCommand
+} from "./runtime/terminal.js";
+
+import {
   connectWS
 } from "./ws_client.js";
 
@@ -12,3 +16,22 @@ connectWS({
   onMessage:
     routeMessage
 });
+
+window.runTerminal =
+  function () {
+
+    const deviceId =
+      document.getElementById(
+        "opcodeDevice"
+      ).value;
+
+    const command =
+      document.getElementById(
+        "terminalInput"
+      ).value;
+
+    executeTerminalCommand({
+      deviceId,
+      command
+    });
+  };
